b8bc310be1612af71e9522b43f5254a834815518
Nico <carlson.nico@gmail.com>
2026-09-01 16:57:28 -0600

    begain the add task function

--- src/commandlinetasktracker/TaskManager.java
+++ src/commandlinetasktracker/TaskManager.java
@@ -6,15 +6,35 @@
 package commandlinetasktracker;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class TaskManager {
 
     // Initialize a scanner.
     Scanner myScan = new Scanner(System.in);
+    ArrayList<Task> taskArrayList = ArrayList<>();
+
+    // initialize ID and completed
+    int taskID = taskArrayList.;        // get length of arraylist for id number
+    boolean taskCompleted = false;
 
     // This function creates a new task.
     public static void addTask() {
+        System.out.println("Enter task name: ");
+        taskTitle = myScan.nextLine();
 
+        System.out.println("Enter task category: ");
+        taskCategory = myScan.nextLine();
+
+        System.out.println("Enter task description: ");
+        taskDescription = myScan.nextLine();
+
+        System.out.println("Enter task priority (1-5): ");
+        taskPriority = Integer.parseInt(myScan.nextLine());
+
+        // generate unique ID
+        // to do - figure out how to get last known task id
+        taskID += 1;
     }
 
     // This function will display a list of all tasks.
