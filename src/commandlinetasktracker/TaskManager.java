cf235513912e6124a7f8154d94c419742baf8c23
pbash25 <parkerbasham@dhcp-192-168-11-154.fvcc.edu>
2026-09-01 16:26:45 -0600

    outlined all main functions of the TaskManager class

--- src/commandlinetasktracker/TaskManager.java
+++ src/commandlinetasktracker/TaskManager.java
@@ -5,5 +5,41 @@
 
 package commandlinetasktracker;
 
+import java.util.Scanner;
+
 public class TaskManager {
+
+    // Initialize a scanner.
+    Scanner myScan = new Scanner(System.in);
+
+    // This function creates a new task.
+    public static void addTask() {
+
+    }
+
+    // This function will display a list of all tasks.
+    public static void listTasks() {
+
+    }
+
+    // This function will update a tasks to completed.
+    public static void completeTask() {
+
+    }
+
+    // This function will allow the user to search task titles.
+    public static void searchTasks() {
+
+    }
+
+    // This function displays statistics to the user.
+    public static void showStats() {
+
+    }
+
+    // This task saves new tasks...
+    public static void saveTasks() {
+
+    }
+
 }
